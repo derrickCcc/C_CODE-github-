58451736ddb05c752f751d1d50915a847d1cfd43
derrict <2644481401@qq.com>
2024-08-08 13:24:40 +0800

    leetcode 80

---
+++ c_8_8/c_8_8/test.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+int removeElement(int* nums, int numsSize, int val)
+{
+    int left = 0;
+    for (int right = 0; right < numsSize; right++)
+    {
+        if (nums[right] != val)
+        {
+            nums[left] = nums[right];
+            left++;
+        }
+    }
+    return left;
+}
