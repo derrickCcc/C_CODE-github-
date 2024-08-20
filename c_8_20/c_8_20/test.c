5ce5e015b2e56718bb604c326bca5fcc732e3db9
derrict <2644481401@qq.com>
2024-08-20 13:13:05 +0800

    leetcode 876

---
+++ c_8_20/c_8_20/test.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* middleNode(struct ListNode* head)
+{
+    struct ListNode* slow = head;
+    struct ListNode* fast = head;
+
+    while (fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
