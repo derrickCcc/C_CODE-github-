7314b171d49a1b502a43cf9e52b9cee7e4044145
derrict <2644481401@qq.com>
2024-08-29 14:23:07 +0800

    leetcode 82

---
+++ c_8_29/c_8_29/test.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* deleteDuplicates(struct ListNode* head)
+{
+    if (!head)
+        return head;
+
+    struct ListNode* dummy = malloc(sizeof(struct ListNode));
+    dummy->next = head;
+
+    struct ListNode* cur = dummy;
+    while (cur->next && cur->next->next)
+    {
+        if (cur->next->val == cur->next->next->val)
+        {
+            int x = cur->next->val;
+            while (cur->next && cur->next->val == x)
+                cur->next = cur->next->next;
+        }
+        else
+            cur = cur->next;
+    }
+    return dummy->next;
+}
